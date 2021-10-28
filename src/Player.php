17def2979d05299ea53f44d9a6c9a9d923f60f9e
Leweyse <darylcastro67@gmail.com>
2021-10-28 10:34:33 +0200

    created Player class

---
+++ src/Player.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+class Player
+{
+    private $cards = [];
+    private $lost = false;
+
+    public function hit() {
+
+    }
+
+    public function surrender() {
+
+    }
+
+    public function getScore() {
+
+    }
+
+    public function hasLost() {
+
+    }
+}
