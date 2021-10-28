4948487dabcc4da5b8d6a898056ab4470861d936
Leweyse <darylcastro67@gmail.com>
2021-10-28 16:57:27 +0200

    testing superglobals

--- index.php
+++ index.php
@@ -10,6 +10,17 @@ require 'src/Deck.php';
 require 'src/Player.php';
 require 'src/Blackjack.php';
 
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
 $deck = new Deck();
 $deck->shuffle();
 // foreach($deck->getCards() AS $card) {
