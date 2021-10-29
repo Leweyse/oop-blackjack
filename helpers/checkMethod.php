90f43faae8a1e8b3cf12c570d1e021d0cf30efe1
Leweyse <darylcastro67@gmail.com>
2021-10-29 17:03:00 +0200

    restructured logic and added variable for components

--- helpers/checkMethod.php
+++ helpers/checkMethod.php
@@ -2,18 +2,70 @@
 declare(strict_types=1);
 
 function checkMethod(&$game, &$player, &$dealer, &$playerScore, &$dealerScore) {
-    if (isset($_POST['hit'])) {
-        $player -> hit($game -> getDeck());
-        $playerScore = $player -> getScore();
-    }
+    global $disabled;
 
-    if (isset($_POST['stand'])) {
-        $dealer -> hit($game -> getDeck());
-        $dealerScore = $dealer -> getScore();
-    }
+    if ($game != null) {
+        if (isset($_POST['hit'])) {
+            $player -> hit($game -> getDeck());
+            $playerScore = $player -> getScore();
 
-    if (isset($_POST['surrender'])) {
-        $player -> surrender();
-        echo "You have lost!";
+            if ($player -> hasLost()) {
+                echo "Player loses!";
+
+                $disabled = true;
+
+                unset($_SESSION['Blackjack']);
+                setBlackjack($game, $player, $dealer, $playerScore, $dealerScore);
+            }
+        }
+
+        if (isset($_POST['stand'])) {
+            $dealer -> hit($game -> getDeck());
+            $dealerScore = $dealer -> getScore();
+
+            if ($dealer -> hasLost()) {
+                echo "Dealer loses!";
+
+                $disabled = true;
+
+                unset($_SESSION['Blackjack']);
+                setBlackjack($game, $player, $dealer, $playerScore, $dealerScore);
+
+            } elseif ($playerScore > $dealerScore) {
+                echo "You win!";
+
+                $disabled = true;
+
+                unset($_SESSION['Blackjack']);
+                setBlackjack($game, $player, $dealer, $playerScore, $dealerScore);
+
+            } elseif ($playerScore < $dealerScore) {
+                echo "Dealer wins!";
+
+                $disabled = true;
+
+                unset($_SESSION['Blackjack']);
+                setBlackjack($game, $player, $dealer, $playerScore, $dealerScore);
+
+            } else {
+                echo "You and Dealer wins!";
+
+                $disabled = true;
+
+                unset($_SESSION['Blackjack']);
+                setBlackjack($game, $player, $dealer, $playerScore, $dealerScore);
+
+            }
+        }
+
+        if (isset($_POST['surrender'])) {
+            $player -> surrender();
+            echo "You lose";
+
+            $disabled = true;
+
+            unset($_SESSION['Blackjack']);
+            setBlackjack($game, $player, $dealer, $playerScore, $dealerScore);
+        }
     }
 }
